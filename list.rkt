8e7ba4f7c1c241c4e5cd178a8a7e6225316eae1f
Irinel Bandas <ibandas@luc.edu>
2019-10-08 22:16:43 -0500

    changes made based off PR, most regarding data definitions and recipe

--- list.rkt
+++ list.rkt
@@ -1,14 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Design the following functions.
-
-
 ; squares : [List-of Number] -> [List-of Number]
 ; Squares each number in the list, producing a new list.
 (check-expect (squares '()) '())
 (check-expect (squares (list 10 0 20)) (list 100 0 400))
 (check-expect (squares (list 1 2 3 4 5)) (list 1 4 9 16 25))
+; Strategy: Structural Decomposition
 (define (squares l)
   (cond
     [(empty? l) '()]
@@ -18,37 +16,43 @@
 ; Determines whether `lst` contains the string "telephone".
 (check-expect (contains-telephone (list "hello" "world" "telephone")) #true)
 (check-expect (contains-telephone (list "hello" "world" "email")) #false)
+; Strategy: Structural Decomposition
 (define (contains-telephone lst)
-  (member "telephone" lst))
+  (cond
+    [(empty? lst) #false]
+    [(string=? (first lst) "telephone") #true]
+    [else (contains-telephone (rest lst))]))
 
 
 ; shortest : [NEList-of String] -> String
 ; Finds the shortest string in `lst`.
-; Recursively goes through the function, compares side by side elements,
-; removes the smaller string until there is one element,
-; and finally extracts the only item left in the list
-; within the empty condition
-; Hint: https://htdp.org/2019-02-24/part_two.html#%28part._sec~3alists~3ane%29
 (check-expect (shortest (list "hello")) "hello")
 (check-expect (shortest (list "saturn" "hello")) "hello")
 (check-expect (shortest (list "mars" "neptune")) "mars")
 (check-expect (shortest (list "mars" "saturn" "neptune")) "mars")
+(check-expect (shortest (list "earth" "pluto" "saturn" "neptune")) "earth")
+; Strategy: Structural Decomposition
 (define (shortest lst)
   (cond
     [(empty? (rest lst)) (first lst)]
     [else (shortest (remove (compare-strings (first lst) (second lst)) lst))]))
 
+; compare-strings: String String -> String
 ; Compares two strings and returns the longest one
 ; Should not pass in two empty strings
+; interp. If two strings are equal, it will just return the second string
 (check-expect (compare-strings "mars" "saturn") "saturn")
 (check-expect (compare-strings "saturn" "neptune") "neptune")
+(check-expect (compare-strings "mercury" "pluto") "mercury")
+(check-expect (compare-strings "earth" "pluto") "pluto")
 ; Strategy : Function Composition
 (define (compare-strings s1 s2)
   (cond
     [( < (string-length s1)
          (string-length s2)) s2]
     [( > (string-length s1)
-         (string-length s2)) s1]))
+         (string-length s2)) s1]
+    [else s2]))
 
 ; mean : [List-of Number] -> Number
 ; Computes the average of the elements of `lst`.
@@ -57,13 +61,17 @@
 (check-expect (mean (list 1 2 3)) 2)
 (check-expect (mean (list 2 4 6 8 10)) 6)
 (check-expect (mean (list 3 7 23 52 70)) 31)
+; Strategy : Structural Composition
 (define (mean lst)
   (cond
   [(empty? lst) 0]
   [else (/ (sum lst) (length lst))]))
 
 ; sum : [List-of Number] -> Number
-; Calculates the sum
+; Calculates the sum of a non-empty list
+(check-expect (sum (list 1)) 1)
+(check-expect (sum (list 1 2 3 4 5)) 15)
+(check-expect (sum (list 23 57 2 17)) 99)
 ; Strategy : Function Composition
 (define (sum lst)
   (cond
--- ravioli.rkt
+++ ravioli.rkt
@@ -1,105 +1,83 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ravioli) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-#|
-Dave’s New Kitchen (on Noyes St. in Evanston) offers ravioli with 3
-fillings (ricotta, butternut squash, lobster) with a choice of 3
-sauces (tomato, cream, wild mushroom).
+;; a Dish is:
+;; (make-dish Filling Sauce)
 
-How could you represent a ravioli filling/sauce combination as data?
-In particular, consider how your representation would allow you to
-define these functions:
+;; a Filling is one of:
+;; - "ricotta"
+;; - "butternut squash"
+;; - "lobster"
 
-
-Design a data-type to represent ravioli dishes that allows you to
-represent any possible ravioli combination.
-
-Then design the four functions described above. Follow the Design Recipe.
-|#
-
-; Struct named "ingredient" that takes in a String String String
-; for the name of the ingredient, the type (filling or sauce)
-; and if it is "vegan" or "not vegan"
-(define-struct ingredient [name type veganity])
-; Fillings
-(define ricotta (make-ingredient "ricotta" "filling" "not-vegan"))
-(define butternut-squash (make-ingredient "butternut-squash" "filling" "vegan"))
-(define lobster (make-ingredient "lobster" "filling" "not-vegan"))
-; Sauces
-(define tomato (make-ingredient "tomato" "sauce" "vegan"))
-(define cream (make-ingredient "cream" "sauce" "not-vegan"))
-(define wild-mushroom (make-ingredient "wild-mushroom" "sauce" "vegan"))
-
-; Struct named "dish" that takes in an ingredient ingredient
-; for filling and sauce
+;; a Sauce is one of:
+;; - "tomato"
+;; - "cream"
+;; - "wild-mushroom"
 (define-struct dish [filling sauce])
 ; Enunmerations - 9 distinct possibilities
 ; 3 combinations using ricotta
-(define DISH1 (make-dish ricotta tomato))
-(define DISH2 (make-dish ricotta cream))
-(define DISH3 (make-dish ricotta wild-mushroom))
+(define DISH1 (make-dish "ricotta" "tomato"))
+(define DISH2 (make-dish "ricotta" "cream"))
+(define DISH3 (make-dish "ricotta" "wild-mushroom"))
 ; 3 combinations using butternut squash
-(define DISH4 (make-dish butternut-squash tomato))
-(define DISH5 (make-dish butternut-squash cream))
-(define DISH6 (make-dish butternut-squash wild-mushroom))
+(define DISH4 (make-dish "butternut-squash" "tomato"))
+(define DISH5 (make-dish "butternut-squash" "cream"))
+(define DISH6 (make-dish "butternut-squash" "wild-mushroom"))
 ; 3 combinations using lobster
-(define DISH7 (make-dish lobster tomato))
-(define DISH8 (make-dish lobster cream))
-(define DISH9 (make-dish lobster wild-mushroom))
+(define DISH7 (make-dish "lobster" "tomato"))
+(define DISH8 (make-dish "lobster" "cream"))
+(define DISH9 (make-dish "lobster" "wild-mushroom"))
 
 
-; `al-pomodoro` takes any filling and returns the combination of that
-;  same filling with tomato sauce.
-
-; Takes in ingredient and returns dish if given a filling
-; If given a ingredient of type sauce or something else
-; it will return "Not a filling"
-(check-expect (al-pomodoro ricotta) DISH1)
-(check-expect (al-pomodoro butternut-squash) DISH4)
-(check-expect (al-pomodoro lobster) DISH7)
-(check-expect (al-pomodoro tomato) "Not a filling")
+; al-pomodoro: Filling -> Dish
+; Takes in a filling and returns a dish with tomato sauce
+(check-expect (al-pomodoro "ricotta") DISH1)
+(check-expect (al-pomodoro "butternut-squash") DISH4)
+(check-expect (al-pomodoro "lobster") DISH7)
+; Strategy: Structural Decomposition
 (define (al-pomodoro filling)
-  (cond
-    [(string=? (ingredient-type filling) "filling")
-     (make-dish filling tomato)]
-    [else "Not a filling"]))
+  (make-dish filling "tomato"))
 
 
-; Pass a dish and return a "vegan" or "not vegan" 
-; `is-vegan?` takes any combination and returns whether that combination
-;  is vegan. Combinations that involve ricotta filling, lobster filling,
-;  or cream sauce are not vegan; all others are.
+; is-vegan?: Dish -> String
+; interp. String will be one of:
+; 1. IS NOT VEGAN (Combinations involving ricotta, lobster, cream)
+; 2. IS VEGAN (Everything else)
 (check-expect (is-vegan? DISH1) "IS NOT VEGAN")
 (check-expect (is-vegan? DISH3) "IS NOT VEGAN")
 (check-expect (is-vegan? DISH4) "IS VEGAN")
 (check-expect (is-vegan? DISH5) "IS NOT VEGAN")
 (check-expect (is-vegan? DISH8) "IS NOT VEGAN")
-; Possibly change else so it doesn't take a non-dish
+; Strategy: Structural Decomposition + Function Composition
 (define (is-vegan? dish)
   (cond
-    [(and (string=? (ingredient-veganity (dish-filling dish)) "vegan")
-          (string=? (ingredient-veganity (dish-sauce dish)) "vegan")) "IS VEGAN"]
-    [else "IS NOT VEGAN"]))
+    [(string=? (dish-filling dish) "ricotta") "IS NOT VEGAN"]
+    [(string=? (dish-filling dish) "lobster") "IS NOT VEGAN"]
+    [(string=? (dish-sauce dish) "cream") "IS NOT VEGAN"]
+    [else "IS VEGAN"]))
 
-
-; `choose` takes a Boolean; given `#true` it returns butternut squash
-;  filling with cream sauce, and given `#false` it returns lobster
-;  filling with wild mushroom sauce.
+; choose: Boolean -> Dish
+; interp. Dish will result in two possibilities:
+; 1. True -> (make-dish "butternut-squash" "cream")
+; 2. False -> (make-dish "lobster" "wild-mushroom")
 (check-expect (choose #true) DISH5)
 (check-expect (choose #false) DISH9)
-(check-expect (choose "hello") "Not given a boolean")
+; Strategy: Structural Decomposition
 (define (choose dish)
   (cond
-    [ (equal? #true dish) DISH5]
-    [ (equal? #false dish) DISH9]
-    [else "Not given a boolean"]))
+    [dish DISH5]
+    [else DISH9]))
 
-; Pass a list and return a list
-; `only-vegan` takes a list of ravioli dishes and returns a list that
-; contains only the vegan dishes from the original list.
+;; a List-of-Dishes is:
+;; - '()
+;; - (cons Dish List-of-Dishes)
+; only-vegan: [List-of-Dishes] -> [List-of-Dishes]
+; interp. Takes in a list of ravioli dishes
+; And returns a list of dishes that are vegan
 (define list-of-dishes (list DISH1 DISH2 DISH3 DISH4 DISH5 DISH6 DISH7 DISH8 DISH9))
 (check-expect (only-vegan list-of-dishes) (list DISH4 DISH6))
 (check-expect (only-vegan '()) '())
+; Strategy: Structural Decomposition
 (define (only-vegan dishes)
   (cond
     [(empty? dishes) '()]
